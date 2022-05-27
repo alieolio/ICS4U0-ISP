dedc86240407b80d4eddb82443f7a206a87363ca
ijessicachen <jessica.chen@leocorn.com>
2022-05-27 14:39:12 -0400

    hover effect added

--- startScreen.java
+++ startScreen.java
@@ -13,16 +13,19 @@
  */
 package com.latter.thelatter;
 import javafx.application.Application;
+
 import javafx.scene.*;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+
 import java.io.FileInputStream;
-public class startScreen extends Application {
+public class startScreen extends Stage {
     /**
      * This method draws and makes the screen for the start, instructions, and exit screen
      * @param primaryStage
@@ -31,6 +34,12 @@ public class startScreen extends Application {
     public void start(Stage primaryStage) throws Exception{
         //making console
         primaryStage.setTitle("Starting Screen");
+
+        /**
+         * credits:
+         * changing based on pseudoclass here (not in a stylesheet)
+         *      https://stackoverflow.com/questions/13074459/javafx-2-and-css-pseudo-classes-setting-hover-attributes-in-setstyle-method
+         */
 
         //adding images
         FileInputStream input = new FileInputStream("startscreen.png");
@@ -45,32 +54,70 @@ public class startScreen extends Application {
         //adding image to background of console
         ImageView imageView = new ImageView(image);
 
+        // shared ColorAdjust for when buttons are hovered
+        ColorAdjust hov = new ColorAdjust();
+        hov.setBrightness(0.2);
+        // when normal
+        ColorAdjust norm = new ColorAdjust();
+        hov.setBrightness(-0.2);
+
         //middle button - 'start' button
         Button buttonM = new Button();
         buttonM.setGraphic(new ImageView(sb));
-        buttonM.setStyle(
-                "-fx-padding: 4 4 4 4;" +
-                "-fx-background-radius: 0;" +
-                "-fx-background-color: #18692e;"
-        );
+        buttonM.setStyle("-fx-padding: 4 4 4 4;" + "-fx-background-radius: 0;" + "-fx-background-color: #18692e;");
+        // way to work with hovered effect here and not in a stylesheet
+        buttonM.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent mouseEvent) {
+                buttonM.getGraphic().setEffect(hov);
+                // change image colour thing here
+            }
+        });
+        buttonM.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent mouseEvent) {
+                buttonM.setStyle("-fx-padding: 4 4 4 4;" + "-fx-background-radius: 0;" + "-fx-background-color: #18692e;");
+                buttonM.getGraphic().setEffect(norm);
+            }
+        });
+
+        /*
+        -fx-padding: 4 4 4 4;
+    -fx-background-radius: 0;
+    -fx-background-color: #18692e;
+         */
 
         //left button - 'instructions' button
         Button buttonL = new Button();
         buttonL.setGraphic(new ImageView(ib));
         buttonL.setTranslateY(20);
-        buttonL.setStyle(
-                "-fx-padding: 4 4 4 4;" +
-                "-fx-background-radius: 0;" +
-                "-fx-background-color: #18692e;");
+        buttonL.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent mouseEvent) {
+                buttonL.getGraphic().setEffect(hov);
+                // change image colour thing here
+            }
+        });
+        buttonL.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent mouseEvent) {
+                buttonL.setStyle("-fx-padding: 4 4 4 4;" + "-fx-background-radius: 0;" + "-fx-background-color: #18692e;");
+                buttonL.getGraphic().setEffect(norm);
+            }
+        });
 
         //right button - 'exit' button
         Button buttonR = new Button();
         buttonR.setGraphic(new ImageView(eb));
         buttonR.setTranslateY(20);
-        buttonR.setStyle(
-                "-fx-padding: 4 4 4 4;" +
-                "-fx-background-radius: 0;" +
-                "-fx-background-color: #18692e;");
+        buttonR.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent mouseEvent) {
+                buttonR.getGraphic().setEffect(hov);
+                // change image colour thing here
+            }
+        });
+        buttonR.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent mouseEvent) {
+                buttonR.setStyle("-fx-padding: 4 4 4 4;" + "-fx-background-radius: 0;" + "-fx-background-color: #18692e;");
+                buttonR.getGraphic().setEffect(norm);
+            }
+        });
 
         //adding buttons and image to hbox to display
         HBox hbox = new HBox(17);
@@ -90,6 +137,8 @@ public class startScreen extends Application {
         root.getChildren().add(sp);
 
         Scene scene = new Scene(root, 512, 393);
+        scene.getStylesheets().add(getClass().getResource("Start.css").toExternalForm());
+
         primaryStage.setScene(scene);
         primaryStage.show();
 
