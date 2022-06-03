72177997a8f03b3aab54f943b3c2d8a4def4d9dd
ijessicachen <jessica.chen@leocorn.com>
2022-06-03 00:45:29 -0400

    buttons work, no button graphics yet
    will be added soon but need to create them first.

--- EndScreen.java
+++ EndScreen.java
@@ -22,72 +22,62 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import java.io.IOException;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;
 
 
-public class EndScreen extends Application {
+public class EndScreen{
 
-    private Stage primaryStage1;
-    private Stage primaryStage2;
+    //private Stage primaryStage1;
+    //private Stage primaryStage2;
 
-    public void start(Stage primaryStage) throws Exception{
-        //making console
-        primaryStage.setTitle("Ending Screen");
-
-        //adding image to background of console
-        FileInputStream input = new FileInputStream("endscreen.png");
-        Image image = new Image(input);
-        ImageView imageView = new ImageView(image);
-
-        //middle button - 'start' button
-        Button buttonM = new Button("Replay");
-        Font font = Font.font("Courier New", FontWeight.BOLD, 35);
-        buttonM.setFont(font);
-        buttonM.setTranslateY(280);
-        //buttonM.setContentDisplay(ContentDisplay.BOTTOM);
-
-        //left button - 'instructions' button
-        Button buttonL = new Button("Credits");
-        Font font2 = Font.font("Courier New", FontWeight.BOLD, 20);
-        buttonL.setFont(font2);
-        buttonL.setTranslateY(297);
-
-        //right button - 'exit' button
-        Button buttonR = new Button("Exit");
-        Font font3 = Font.font("Courier New", FontWeight.BOLD, 25);
-        buttonR.setFont(font3);
-        buttonR.setTranslateY(290);
-
-        //adding buttons and image to hbox to display
-        HBox hbox = new HBox(17);
-        hbox.setTranslateX(45);
-        hbox.getChildren().add(buttonL);
-        hbox.getChildren().add(buttonM);
-        hbox.getChildren().add(buttonR);
-
-        /**
-         * <a href="https://stackoverflow.com/questions/27912628/how-to-overlap-buttons-text-over-an-image-with-javafx-8 site </a> (01/12/2015)
-         */
+    public HBox endS(Button buttonM, Button buttonL, Button buttonR){
         StackPane sp = new StackPane();
-        sp.getChildren().addAll(imageView, hbox);
+
+        try{
+            //adding image to background of console
+            FileInputStream input = new FileInputStream("endscreen.png");
+            Image image = new Image(input);
+            ImageView imageView = new ImageView(image);
+
+            //middle button - 'start' button
+            Font font = Font.font("Courier New", FontWeight.BOLD, 35);
+            buttonM.setFont(font);
+            buttonM.setTranslateY(280);
+            //buttonM.setContentDisplay(ContentDisplay.BOTTOM);
+
+            //left button - 'instructions' button
+            Font font2 = Font.font("Courier New", FontWeight.BOLD, 20);
+            buttonL.setFont(font2);
+            buttonL.setTranslateY(297);
+
+            //right button - 'exit' button
+            Font font3 = Font.font("Courier New", FontWeight.BOLD, 25);
+            buttonR.setFont(font3);
+            buttonR.setTranslateY(290);
+
+            //adding buttons and image to hbox to display
+            HBox hbox = new HBox(17);
+            hbox.setTranslateX(45);
+            hbox.getChildren().add(buttonL);
+            hbox.getChildren().add(buttonM);
+            hbox.getChildren().add(buttonR);
+
+            /**
+             * <a href="https://stackoverflow.com/questions/27912628/how-to-overlap-buttons-text-over-an-image-with-javafx-8 site </a> (01/12/2015)
+             */
+            sp.getChildren().addAll(imageView, hbox);
+        }catch (IOException e){
+        }
 
         HBox root = new HBox();
         root.getChildren().add(sp);
 
-        Scene scene = new Scene(root, 512, 393);
-        primaryStage.setScene(scene);
-
-        primaryStage.show();
+        return root;
     }
 
-    /**
-     * Main method to run the 'start' method
-     * @param args
-     */
-    public static void main(String[] args) {
-        Application.launch(args);
-    }
+
 }
 
