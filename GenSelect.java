bf358e66e11d5ee313d6cdcbd53f8fc5d37ff20a
ijessicachen <jessica.chen@leocorn.com>
2022-06-03 14:07:45 -0400

    Update GenSelect.java

--- GenSelect.java
+++ GenSelect.java
@@ -14,7 +14,6 @@ package com.latter.thelatter;
 import javafx.application.Application;
 import javafx.scene.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
