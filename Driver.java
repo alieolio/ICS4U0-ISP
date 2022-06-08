1233dc4a65f033b401c3f22932c3ebcb4b8756ca
ijessicachen <jessica.chen@leocorn.com>
2022-06-08 12:34:54 -0400

    works with movement and office rooms
    kind of, also more sources added

--- Driver.java
+++ Driver.java
@@ -1,16 +1,19 @@
 package com.latter.thelatter;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.*;
 import javafx.scene.control.Button;
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.scene.input.KeyEvent;
 
-public class Driver extends Application{
+public class Driver extends Movement{
+
     public void start(Stage stage) throws Exception {
-
         // BUTTONS
         // start screen
         Button buttonM = new Button();
@@ -41,6 +44,25 @@ public class Driver extends Application{
         GenSelect g = new GenSelect();
         Scene gs = new Scene(g.genSel(bc, bm, bw, bgq), 512, 393);
         gs.getStylesheets().add(getClass().getResource("GenSel.css").toExternalForm());
+        // workplace screen
+        Workplace w = new Workplace();
+        Scene d1 = new Scene(w.office(), 512, 393);
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                movement(w.player, w.enterFunc, w.enterFunc2, "office");
+            }
+        };
+        // boss' office screen
+        Boss bo = new Boss();
+        Scene p1 = new Scene(bo.bossO(), 512, 393);
+        AnimationTimer bT = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                movement(bo.player, bo.enterFunc, bo.enterFunc2, "boss");
+            }
+        };
+
         // end screen
         EndScreen e = new EndScreen();
         Scene end = new Scene(e.endS(ebuttonM, ebuttonL, ebuttonR), 512, 393);
@@ -56,9 +78,10 @@ public class Driver extends Application{
          * <ul> 
          *     <li href="https://stackoverflow.com/questions/12804664/how-to-swap-screens-in-a-javafx-application-in-the-controller-class"> swapping screens part 1 </li>
          *     <li href="https://stackoverflow.com/questions/49287392/javafx-how-to-change-scene-using-a-scene-from-another-class"> swapping screens part 2 </li>
-         *     <li> there should be a part three </li>
-         *     <li href="https://www.cs.scranton.edu/~mccloske/courses/com_line_args_jGrasp.html"> r
-unning javaFX on jGRASP -command line arguments </li>
+         *     <li href="https://stackoverflow.com/questions/36551431/can-you-write-two-different-java-fx-scenes-as-two-separate-classes"> swapping screens part 3 </li>
+         *     <li href="https://stackoverflow.com/questions/43086875/javafx-8-on-button-click-show-other-scene"> swapping screens with buttons </li>
+         *     <li href="https://stackoverflow.com/questions/26619566/javafx-stage-close-handler"> closing screen </li>
+         *     <li href="https://www.cs.scranton.edu/~mccloske/courses/com_line_args_jGrasp.html"> running javaFX on jGRASP -command line arguments </li>
          *     <li href="https://stackoverflow.com/questions/54662129/javafx-11-javafx-runtime-components-are-missing"> running javaFX on jGRASP -compiler settings </li>
          */
         // start screen
@@ -95,10 +118,56 @@ unning javaFX on jGRASP -command line arguments </li>
         bc.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                stage.setScene(end);
+                stage.setScene(d1);
+                timer.start();
             }
         });
+
+        // deficiency 1
+        d1.setOnKeyPressed(new EventHandler<KeyEvent>() { //moves the character when the correct key is pressed
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                boolean state = velStart(keyEvent);
+                setVelocity();
+                if (getClear() && state){ //just key for now, not checking location
+                    timer.stop();
+                    bT.start();
+                    stage.setScene(p1);
+                }
+            }
+        });
+        d1.setOnKeyReleased(new EventHandler<KeyEvent>() { //stops the character when the correct key is released
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                velStop(keyEvent);
+            }
+        });
+
+        // panic 1
+        p1.setOnKeyPressed(new EventHandler<KeyEvent>() { //moves the character when the correct key is pressed
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                boolean state = velStart(keyEvent);
+                setVelocity();
+                if (getClear() && state){ //just key for now, not checking location
+                    stage.setScene(end);
+                }
+            }
+        });
+        p1.setOnKeyReleased(new EventHandler<KeyEvent>() { //stops the character when the correct key is released
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                velStop(keyEvent);
+            }
+        });
+
+
         // end screen
+        /*
+        YOU NEED TO FIND A WAY TO ACTUALLY RESTART ALL SCENES,
+        SINCE MANY WILL BE IN THE STATE THEY WRE LEFT, WE DON'T
+        WANT THAT
+         */
         ebuttonM.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
