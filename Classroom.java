98e037856a4efdaddd2adaa5becaddfaf9efc5f7
ijessicachen <jessica.chen@leocorn.com>
2022-06-10 13:58:52 -0400

    facts work

--- Classroom.java
+++ Classroom.java
@@ -1,34 +1,58 @@
+/**
+ * The Latter, a game aimed to spread awareness about the adversities of gender inequality
+ * <h2>Course Info:</h2>
+ * ICS4U0 wit Krasteva V.
+ * <p>
+ * Version 1 - 05.30.2022
+ * Created working code for movement, however it is not smooth
+ *
+ * Version 2 - 05.30.2022
+ * Made the movement fluid and 8 directional
+ * <p>
+ * Real code that moves the character around a console/screen
+ *
+ * @version 05.30.22
+ * @author Artemis Chen and Alicia Chung
+ */
 package com.latter.thelatter;
 
-import javafx.scene.control.Button;
+import javafx.scene.Group;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.Group;
-import javafx.scene.layout.*;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.stage.Stage;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Classroom{
 
+    /** code taken and adapted from
+     * <a href="https://gist.github.com/jewelsea/8321740">Response to a questions about sprite movement</a> (01/08/2013)
+     * <a href="https://zetcode.com/javagames/movingsprites/">Moving sprites</a>
+     */
+
     ImageView background = new ImageView();
     ImageView player = new ImageView();
     ImageView enterFunc = new ImageView();
     ImageView enterFunc2 = new ImageView();
+    ImageView instructions2 = new ImageView();
+    ImageView viewF = new ImageView();
+    // fact screens
+    ImageView f1 = new ImageView();
+    ImageView f2 = new ImageView();
+    ImageView f3 = new ImageView();
+    ImageView f4 = new ImageView();
 
-    public Group classR(){
+    public Group classR () {
         Group group = new Group();
-        try{
+        try {
             // add background
-            FileInputStream input = new FileInputStream("Class.png");
-            Image image = new Image(input);
-            background.setImage(image);
+            FileInputStream bg = new FileInputStream("Class.png");
+            Image a1 = new Image(bg);
+            background = new ImageView(a1);
             group.getChildren().add(background);
             // other graphics
             FileInputStream playerImage = new FileInputStream("player.png");
-            image = new Image(playerImage);
+            Image image = new Image(playerImage);
             player.setImage(image);
             FileInputStream ent = new FileInputStream("enter button.png");
             Image p = new Image(ent);
@@ -36,26 +60,98 @@ public class Classroom{
             FileInputStream ent2 = new FileInputStream("enter button.png");
             Image p2 = new Image(ent2);
             enterFunc2.setImage(p2);
+            FileInputStream instruc = new FileInputStream("part 2.png");
+            Image i = new Image(instruc);
+            instructions2 = new ImageView(i);
+            FileInputStream img = new FileInputStream("view button.png");
+            image = new Image(img);
+            viewF.setImage(image);
+            //fact dump
+            img = new FileInputStream("facts and questions/education 1.png");
+            image = new Image(img);
+            f1.setImage(image);
+            img = new FileInputStream("facts and questions/education 2.png");
+            image = new Image(img);
+            f2.setImage(image);
+            img = new FileInputStream("facts and questions/education 3.png");
+            image = new Image(img);
+            f3.setImage(image);
+            img = new FileInputStream("facts and questions/education 4.png");
+            image = new Image(img);
+            f4.setImage(image);
 
             // positioning enters
             group.getChildren().add(enterFunc);
             enterFunc.setVisible(false);
             enterFunc.setX(0);
             enterFunc.setY(-5);
-
             group.getChildren().add(enterFunc2);
             enterFunc2.setVisible(false);
             enterFunc2.setX(412);
             enterFunc2.setY(364);
 
+
+            // view facts notice
+            group.getChildren().add(viewF);
+            viewF.setVisible(false);
+            viewF.relocate(0, 0);
+
             // adding player to screen
             group.getChildren().add(player);
-            player.relocate(20, 10);
+            player.setX(237);
+            player.setY(300);
+
+            // facts to scren
+            group.getChildren().add(f1);
+            f1.setVisible(false);
+            f1.relocate(12, 12);
+            group.getChildren().add(f2);
+            f2.setVisible(false);
+            f2.relocate(12, 12);
+            group.getChildren().add(f3);
+            f3.setVisible(false);
+            f3.relocate(12, 12);
+            group.getChildren().add(f4);
+            f4.setVisible(false);
+            f4.relocate(12, 12);
+
+            instructions2.setVisible(true);
+            instructions2.setX(12);
+            instructions2.setY(12);
+            group.getChildren().add(instructions2);
 
         } catch (IOException e){
+
         }
 
+        System.out.println("in classroom " + group.getChildren().toString());
+        System.out.println();
         return group;
     }
 
+    public void setInsOff() {
+        instructions2.setVisible(false);
+    }
+    public void showF (int fact){
+        switch (fact){
+            case 1:
+                f1.setVisible(true);
+                break;
+            case 2:
+                f2.setVisible(true);
+                break;
+            case 3:
+                f3.setVisible(true);
+                break;
+            case 4:
+                f4.setVisible(true);
+                break;
+            default:
+                f1.setVisible(false);
+                f2.setVisible(false);
+                f3.setVisible(false);
+                f4.setVisible(false);
+                break;
+        }
+    }
 }
